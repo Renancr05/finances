76db87a86bd8b3348822573459d50af0bb876ac0
Renancr05 <superrenanxxx@gmail.com>
2026-05-20 17:50:24 -0300

    update

--- lib/main.dart
+++ lib/main.dart
@@ -17,7 +17,7 @@ class BancoDigitalApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Banco Digital',
+      title: 'Aplex Bank',
       debugShowCheckedModeBanner: false,
       theme: ThemeData(hintColor: Colors.green, primaryColor: Colors.white),
       initialRoute: '/',
